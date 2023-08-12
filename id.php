dc6dd8ec15a25d9c528d0b1e3e547e6c92cf10b1
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2023-08-12 18:13:46 +0600

    Create id.php

---
+++ id.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Madrasatu Ibadir Rahman</title>
+  <script type="text/javascript">
+    window.onload = function() {
+      // Replace 'https://www.example.com' with the URL you want to redirect to
+      window.location.href = 'http://103.85.156.3:100/idlist.php';
+    };
+  </script>
+</head>
+<body>
+  <p>This page will automatically redirect after loading.</p>
+</body>
+</html>
